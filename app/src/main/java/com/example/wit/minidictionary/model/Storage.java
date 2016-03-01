c725b1bd7eba970704e6ab2d1e9c8d2687807820
Norawit Urailertprasert <wit543@gmail.com>
2016-03-02 00:21:01 +0700

    add storage

---
+++ app/src/main/java/com/example/wit/minidictionary/model/Storage.java
@@ -0,0 +1,36 @@
+package com.example.wit.minidictionary.model;
+
+import com.example.wit.minidictionary.word.Word;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by WIT on 02-Mar-16.
+ */
+public class Storage {
+    private List<Word> words;
+    private static Storage storage;
+    private Storage(){
+        words = new ArrayList<Word>();
+    }
+    public static Storage getInstance(){
+        if(storage==null)
+            storage = new Storage();
+        return storage;
+    }
+    public void addWord(Word word){
+        words.add(word);
+    }
+    public void deleteWord(Word word){
+        words.remove(word);
+    }
+    public void deleteWord(String word){
+        for(Word w:words)
+            if(w.getWord().equals(word))
+                words.remove(w);
+    }
+    public List<Word> loadWord(){
+        return words;
+    }
+}
